a0143b0636e78c103f5c194a653315bb0deeac52
KadenSeaman <kadencs24@outlook.com>
2025-10-03 23:27:48 -0600

    pong

--- app/main.ts
+++ app/main.ts
@@ -3,8 +3,13 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
-const server: net.Server = net.createServer((connection: net.Socket) => {
-   //Handle connection;
- });
+const server: net.Server = net.createServer((socket: net.Socket) => {
+  socket.on('data', (data) => {
+    const msg = data.toString();
+    if (msg === 'PING') {
+      socket.write('+PONG\r\n')
+    }
+  })
+});
 
- server.listen(6379, "127.0.0.1");
+server.listen(6379, "127.0.0.1");
