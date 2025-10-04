746bccc248f2e070fa86a083a72a5fd177cc8e0e
KadenSeaman <kadencs24@outlook.com>
2025-10-03 23:44:28 -0600

    test

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,6 @@ import * as net from "net";
 
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((socket: net.Socket) => {
   socket.on('data', () => {
     socket.write('+PONG\r\n')
