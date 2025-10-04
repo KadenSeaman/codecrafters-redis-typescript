a33229f6406288125b9e029ba62bf4344d2b6296
KadenSeaman <kadencs24@outlook.com>
2025-10-03 23:08:25 -0600

    pass

--- app/main.ts
+++ app/main.ts
@@ -4,7 +4,7 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
-   Handle connection
+   //Handle connection;
  });
 
  server.listen(6379, "127.0.0.1");
